e4bf5051b2ff6e6106c2cafc964f19a3c37388d7
Priya Shah <priya.shah1545@gmail.com>
2022-08-19 20:14:07 -0600

    Create Palindrome.py

---
+++ Strings/Palindrome.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # n = len(str(x))
+        # x = str(x)
+        # half = n/2
+        # for i in range(half):
+        #     print(x[i])
+        #     for j in range(n-1,half+1,-1):
+        #         first = x[i]
+        #         last = x[j]
+        #         print(last)
+        #         if first == last:
+        #             i=i+1
+        #         else:
+        #             return False
+        #         if j == half:
+        #             return True
+        x= str(x)
+        reverse = x[::-1]
+        if str(x) == reverse:
+            return True
+        else:
+            return False
+        
